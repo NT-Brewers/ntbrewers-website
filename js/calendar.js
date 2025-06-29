cca371a9c4553ce1c2724f7a0095e3a9c71a6472
Elvey <elvey@Elveys-MacBook-Air.local>
2025-06-29 17:18:38 +0930

    feat: add calendar and hide extra events

---
+++ js/calendar.js
@@ -0,0 +1,27 @@
+import { Calendar }             from '@fullcalendar/core';
+import dayGridPlugin            from '@fullcalendar/daygrid';
+import icalendarPlugin          from '@fullcalendar/icalendar';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // hide past events
+  const now = new Date();
+  Array.from(document.querySelectorAll("event-item__item")).forEach((eventItem) => {
+      const eventDate = new Date(eventItem.dataset.date);
+      if(eventDate < now) {
+          eventItem.remove();
+      }
+  });
+
+  // populate the calendar
+  const cal = new Calendar(document.getElementById('club-calendar'), {
+    plugins: [dayGridPlugin, icalendarPlugin],
+    timeZone: 'Australia/Darwin',
+    initialView: 'dayGridMonth',
+    events: {
+      url: '/events.ics',
+      format: 'ics'
+    }
+  });
+  cal.render();
+});
